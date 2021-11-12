bacdf859a4a448e0580d2e3c5e25ca1a91ddd65b
FoxSide <foxside8452@gmail.com>
2021-11-12 18:59:13 +0200

    19ln

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,14 +1,15 @@
 import React from "react";
 import s from './Nav.module.css'
+import {NavLink} from "react-router-dom";
 
 export const Nav = () => {
   return (
     <nav className={s.nav}>
-      <div className={s.item}><a href={'/profile'}>Profile</a></div>
-      <div className={s.item}><a href={'/message'}>Messages</a></div>
-      <div className={s.item}><a href={'/news'}>News</a></div>
-      <div className={s.item}><a href={'/music'}>Music</a></div>
-      <div className={s.item}><a href={'/settings'}>Settings</a></div>
+      <div className={s.item}><NavLink to='/profile'>Profile</NavLink></div>
+      <div className={s.item}><NavLink to='/message'>Messages</NavLink></div>
+      <div className={s.item}><NavLink to='/news'>News</NavLink></div>
+      <div className={s.item}><NavLink to='/music'>Music</NavLink></div>
+      <div className={s.item}><NavLink to='/settings'> Settings</NavLink></div>
     </nav>
   )
 }
