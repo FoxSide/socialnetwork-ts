0f04e11a8f6c96e37b13d2362e0bf76bad6a7498
FoxSide <foxside8452@gmail.com>
2021-11-14 22:46:20 +0200

    22ln

--- src/App.tsx
+++ src/App.tsx
@@ -18,11 +18,11 @@ export const App = () => {
           <Nav/>
           <div className={s.appWrapperContent}>
             <Routes>
-            <Route path={'/profile'} element={<Profile/>}/>
-            <Route path={'/dialogs/'} element={<Dialogs/>}/>
-            <Route path={'/news'} element={<News/>}/>
-            <Route path={'/music'} element={<Music/>}/>
-            <Route path={'/settings'} element={<Settings/>}/>
+            <Route path='/profile' element={<Profile/>}/>
+            <Route path='/dialogs' element={<Dialogs/>}/>
+            <Route path='/news' element={<News/>}/>
+            <Route path='/music' element={<Music/>}/>
+            <Route path='/settings' element={<Settings/>}/>
             </Routes>
           </div>
         </div>
--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -6,13 +6,13 @@ export const Dialogs = () => {
   return (
     <div className={s.dialogs}>
       <div className={s.user}>
-        <div><NavLink to={'/dialogs/1'}>Ed</NavLink></div>
-        <div><NavLink to={'/dialogs/2'}>Viktoria</NavLink></div>
-        <div><NavLink to={'/dialogs/3'}>Ned</NavLink></div>
-        <div><NavLink to={'/dialogs/4'}>Varova</NavLink></div>
+        <div><NavLink to='/dialogs/1'>Ed</NavLink></div>
+        <div><NavLink to='/dialogs/2'>Viktoria</NavLink></div>
+        <div><NavLink to='/dialogs/3'>Ned</NavLink></div>
+        <div><NavLink to='/dialogs/4'>Varova</NavLink></div>
       </div>
       <div className={s.message}>
-        <div>Hello</div>
+        <div>Hello!</div>
         <div>How are you?</div>
         <div>:)</div>
       </div>
