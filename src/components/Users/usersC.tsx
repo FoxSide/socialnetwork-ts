5413e3c00bc2b8c249cd9ac07da7442f227325f3
FoxSide <foxside8452@gmail.com>
2022-01-10 16:11:09 +0200

    54ln

--- src/components/Users/usersC.tsx
+++ src/components/Users/usersC.tsx
@@ -17,21 +17,23 @@ export class Users extends React.Component<UserspropsType> {
   render() {
     return (
       <div>
-        {this.props.users.map(u => <div key={u.id}>
-          <div>
-            <img src={u.photos.small !== null ? u.photos.small : userPhoto} alt="ava" className={s.photo}/>
+        {this.props.users.map(u => <div key={u.id} className={s.body}>
+          <div className={s.logo}>
+            <div>
+              <img src={u.photos.small !== null ? u.photos.small : userPhoto} alt="ava" className={s.photo}/>
+            </div>
+            <div>
+              {u.followed
+                ? <button onClick={() => {
+                  this.props.unFollow(u.id)
+                }}>Unfollow</button>
+                : <button onClick={() => {
+                  this.props.follow(u.id)
+                }}>Follow</button>}
+            </div>
           </div>
-          <div>
-            {u.followed
-              ? <button onClick={() => {
-                this.props.unFollow(u.id)
-              }}>Unfollow</button>
-              : <button onClick={() => {
-                this.props.follow(u.id)
-              }}>Follow</button>}
-          </div>
-          <div>
-            <div>{u.name}</div>
+          <div className={s.content}>
+            <div>Name: {u.name}</div>
           </div>
         </div>)}
       </div>
